419e4f01184f1ba481f25eb80356e4d63e95b9a8
0x13 <dilucrag19@gmail.com>
2025-02-22 22:43:24 +0800

    Make footer fonts bigger in smaller devices

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-2 sm:py-3 md:py-4 text-center">
       <div className="container mx-auto">
-        <p className="text-[0.5rem] sm:text-base">&copy; {new Date().getFullYear()} IT343-3B Group 5 Activity. All rights reserved.</p>
+        <p className="text-xs sm:text-base">&copy; {new Date().getFullYear()} IT343-3B Group 5 Activity. All rights reserved.</p>
       </div>
     </footer>
   );
